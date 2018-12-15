136c8fc9f3a0772d011d9dde3b872dbe9eff7184
vokic <30930109+vokic@users.noreply.github.com>
2018-12-15 13:13:39 +0100

    Add files via upload

---
+++ objekti.js
@@ -0,0 +1,322 @@
+const korisnici = [
+
+    {
+        "_id": "5c13d693f334db1e4080e8ae",
+        "index": 0,
+        "guid": "c06e6cbf-9558-41f5-8c2f-d702162129eb",
+        "isActive": true,
+        "balance": "$3,918.88",
+        "picture": "https://source.unsplash.com/200x200/?people",
+        "age": 37,
+        "eyeColor": "green",
+        "name": "Lott Russo",
+        "gender": "female",
+        "company": "EZENTIA",
+        "email": "lottrusso@ezentia.com",
+        "phone": "+1 (992) 553-3582",
+        "address": "819 Degraw Street, Camptown, North Carolina, 6452",
+        "about": "Cupidatat occaecat incididunt in qui magna. Tempor non elit incididunt amet tempor ea. Qui quis in magna cupidatat cillum mollit voluptate enim duis ex culpa voluptate ad labore. Tempor Lorem cillum eu ex officia excepteur dolor esse. Ullamco eu culpa excepteur in incididunt irure eiusmod cillum non esse. Mollit ullamco irure aute nulla mollit velit labore et minim cupidatat laborum mollit do minim. Esse minim anim laboris ad ad laboris exercitation laboris.\r\n",
+        "registered": "2014-11-15T07:44:31 -01:00",
+        "latitude": -50.761374,
+        "longitude": -107.812051,
+        "tags": [
+            "exercitation",
+            "do",
+            "laborum",
+            "velit",
+            "in",
+            "ullamco",
+            "in"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Eula Ayers"
+            },
+            {
+                "id": 1,
+                "name": "Lisa Phillips"
+            },
+            {
+                "id": 2,
+                "name": "Salas Knowles"
+            }
+        ]
+    },
+    {
+        "_id": "5c13d693fffbd9416017c8df",
+        "index": 1,
+        "guid": "79311069-14e6-46dd-b457-378dece7ed49",
+        "isActive": false,
+        "balance": "$3,548.03",
+        "picture": "https://source.unsplash.com/200x200/?girl",
+        "age": 35,
+        "eyeColor": "blue",
+        "name": "Roberson Vance",
+        "gender": "male",
+        "company": "ISOTRONIC",
+        "email": "robersonvance@isotronic.com",
+        "phone": "+1 (829) 447-3082",
+        "address": "737 Huron Street, Bonanza, Ohio, 511",
+        "about": "Fugiat deserunt ullamco deserunt fugiat. Elit officia id culpa amet ex excepteur. Sint elit consequat commodo aute anim in. Do non anim id ex duis fugiat. Mollit aute laboris labore do cillum adipisicing. Eu enim tempor tempor fugiat est qui voluptate ex excepteur.\r\n",
+        "registered": "2015-07-13T10:50:20 -02:00",
+        "latitude": -67.805599,
+        "longitude": 125.819444,
+        "tags": [
+            "tempor",
+            "veniam",
+            "dolor",
+            "nisi",
+            "Lorem",
+            "amet",
+            "aliqua"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Lynne Johnston"
+            },
+            {
+                "id": 1,
+                "name": "Virginia Pacheco"
+            },
+            {
+                "id": 2,
+                "name": "Wiggins Dunn"
+            }
+        ]
+    },
+    {
+        "_id": "5c13d69312981b46231f1fd2",
+        "index": 2,
+        "guid": "e2450d48-6702-4eb0-88bd-adc30ba8da70",
+        "isActive": true,
+        "balance": "$3,847.54",
+        "picture": "https://source.unsplash.com/200x200/?people,girl",
+        "age": 32,
+        "eyeColor": "red",
+        "name": "Nash Gardner",
+        "gender": "male",
+        "company": "TURNLING",
+        "email": "nashgardner@turnling.com",
+        "phone": "+1 (866) 467-3722",
+        "address": "714 Bowery Street, Bloomington, Vermont, 2704",
+        "about": "Dolore veniam cupidatat aute amet quis minim consequat in cupidatat. Consectetur do occaecat sit labore incididunt non laboris consequat est. Esse elit velit enim nulla nisi pariatur culpa labore eu labore duis eiusmod. Cupidatat id anim cupidatat dolor ipsum velit ea.\r\n",
+        "registered": "2016-05-21T04:05:48 -02:00",
+        "latitude": 41.713319,
+        "longitude": 147.297528,
+        "tags": [
+            "enim",
+            "eiusmod",
+            "aliqua",
+            "non",
+            "pariatur",
+            "qui",
+            "mollit"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Selena Davenport"
+            },
+            {
+                "id": 1,
+                "name": "Monroe Zamora"
+            },
+            {
+                "id": 2,
+                "name": "Kris Gallegos"
+            }
+        ]
+    },
+    {
+        "_id": "5c13d69317a59cdb8504455c",
+        "index": 3,
+        "guid": "7991ca32-8a72-4d6f-98e3-cf92fe03176a",
+        "isActive": false,
+        "balance": "$3,285.78",
+        "picture": "https://source.unsplash.com/200x200/?people,boy",
+        "age": 31,
+        "eyeColor": "black",
+        "name": "Debora Atkins",
+        "gender": "female",
+        "company": "TUBESYS",
+        "email": "deboraatkins@tubesys.com",
+        "phone": "+1 (928) 541-2411",
+        "address": "549 High Street, Wanship, New York, 9593",
+        "about": "Laborum proident magna magna laboris sunt cupidatat ex sint sint magna Lorem. Incididunt irure incididunt occaecat aliqua Lorem aliquip. Anim consequat non do cillum. Culpa non labore eiusmod incididunt amet consectetur dolore occaecat ad.\r\n",
+        "registered": "2017-02-19T03:24:49 -01:00",
+        "latitude": -27.780949,
+        "longitude": -36.625173,
+        "tags": [
+            "tempor",
+            "adipisicing",
+            "incididunt",
+            "proident",
+            "magna",
+            "quis",
+            "id"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Kerri Osborne"
+            },
+            {
+                "id": 1,
+                "name": "Gwendolyn George"
+            },
+            {
+                "id": 2,
+                "name": "Nguyen Newton"
+            }
+        ]
+    },
+    {
+        "_id": "5c13d69319c3aa4fb02ba420",
+        "index": 4,
+        "guid": "3f7cce9a-34e9-4514-a7b2-030591fa914e",
+        "isActive": false,
+        "balance": "$3,114.06",
+        "picture": "https://source.unsplash.com/200x200/?girl, girl",
+        "age": 39,
+        "eyeColor": "green",
+        "name": "Jackson Knight",
+        "gender": "male",
+        "company": "SONIQUE",
+        "email": "jacksonknight@sonique.com",
+        "phone": "+1 (991) 405-3877",
+        "address": "401 Grimes Road, Manila, Oklahoma, 7209",
+        "about": "Sint qui cillum magna tempor sit non. Irure incididunt nisi cillum esse nisi sunt aute fugiat adipisicing adipisicing laboris voluptate aute aliqua. Proident in ullamco sit sunt quis exercitation sint occaecat ut pariatur reprehenderit nostrud occaecat.\r\n",
+        "registered": "2014-04-02T08:43:01 -02:00",
+        "latitude": 16.491082,
+        "longitude": 6.029525,
+        "tags": [
+            "elit",
+            "Lorem",
+            "esse",
+            "laborum",
+            "deserunt",
+            "ipsum",
+            "quis"
+        ],
+        "friends": [
+            {
+                "id": 0,
+                "name": "Dawn Mcguire"
+            },
+            {
+                "id": 1,
+                "name": "Beulah Lang"
+            },
+            {
+                "id": 2,
+                "name": "Karyn Wilcox"
+            }
+        ]
+    }
+]
+
+const root = document.getElementById("root")
+/*
+for (let i = 0; i < korisnici.length; i++) {
+    console.log(korisnici[i].name);
+
+}
+
+//prosecna starost korisnika
+
+/*
+let zbir = 0
+for (let i = 0; i < korisnici.length; i++) {
+    zbir += korisnici[i].age
+}
+let prosecnaStarost = zbir / korisnici.length
+
+//da li postoji osoba po imenu "Lott Russo"
+
+const trazenoIme = "Lott Russo"
+let nadjeno = false
+for (var i = 0; i < korisnici.length; i++) {
+    if (korisnici[i].name == trazenoIme) nadjeno = true
+
+}
+
+//da li postoji osoba po imenu samo "Lott"
+
+if (korisnici[i].name.includes("Lott"))
+    console.log('Postoji ime Lott pod indeksom', i)
+
+// konvertovati format $3,241.09 u 3241.09
+
+//Eloquent Java Script Marin Hupfbekel
+
+/* var broj = korisnici[i].balance
+for (let i = 0; i < korisnici.length; i++) {
+    cistBroj = bezDecimala.replace(/[^0-9.-]+/g, "")
+    bezDecimala += Number(broj)
+} */
+
+/* ukupno = 0
+zapocinjemo iteraciju
+pristupamo polju plata
+normalizujemo podatak
+    izbaciti dolar
+    izbaciti zarez
+    pretvoriti strunu u broj
+dodajemo platu na ukupno
+
+delimo ukupno sa brojem korisnika
+ispisati rezultat */
+
+let ukupno = 0
+
+
+for (let i = 0; i < korisnici.length; i++) {
+    const user = korisnici[i]
+   // const plata = Number(korisnici[i].balance.replace('$', '').replace(',', ''))
+   // ukupno += plata
+
+   let ukupno = 0
+   const uclanjen = new Date(user.registered.split(' ')[0])
+   const danas = new Date()
+   const kolikoJeClan = danas - uclanjen
+
+   let dana = kolikoJeClan / 1000 / 60 / 60 / 24
+   const godina = Math.floor(dana / 365)
+   dana = dana % 365
+   const meseci = Math.floor(dana / 30)
+   dana = Math.floor(dana % 30)
+
+   //console.log(godina, meseci, dana)
+
+const infoGodina = godina ? godina + " godine " :  ""
+const infoMEseci = meseci ? meseci + " meseci " :  ""
+const infoDana = dana ? "i " + dana + " dana" : ""
+
+const uclanjenje = user.gender == "male" ? "Uclanjen" : "Uclanjena"
+const poruka = `${uclanjenje} pre ${infoGodina}${infoMEseci}${infoDana}.`
+
+    const sablon = `
+<div class="korisnik">
+<h3 style="color: ${user.eyeColor}">${user.name}</h1>
+<img src=${user.picture}>
+<p> ${poruka}</p>
+<p>Starost: ${user.age} godina</p>
+<p>Zaposlen u: ${user.company}</p>
+<p>Plata: ${user.balance}</p>
+<p>Status: ${user.isActive ? "aktivan" : "neaktivan"}</p>
+<p>Pol: ${user.gender == "male" ? 'Muski' : 'Zenski'}</p>
+<p>✉ <a href="mailto:"> ${user.email}</a></p>
+<p>☎ <a href="tel: ${user.phone}">${user.phone}</a></p>
+<p>🗺 <a href="https://www.google.com/maps/search/?q=${user.latitude},${user.longitude}" target="_blank"> Lokacija korisnika</a></p>
+<iframe width="100%" height="300px" src="https://maps.google.com/maps?q=${user.latitude},${user.longitude}&output=embed&z=3"></iframe>
+
+</div>
+`
+
+root.innerHTML += sablon
+
+}
+// const prosek = ukupno / korisnici.length
+// root.innerHTML += "Prosecna plata korisnika je: " + prosek.toFixed(2) + " dolara"
